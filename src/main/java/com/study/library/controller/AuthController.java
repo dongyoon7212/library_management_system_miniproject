c4a8da43d7643f81c6a5f17b6f7630752cd9d5ad
dongyoon7212 <dongyoon7212@naver.com>
2024-03-14 10:12:48 +0900

    feat: Valid Pattern

--- src/main/java/com/study/library/controller/AuthController.java
+++ src/main/java/com/study/library/controller/AuthController.java
@@ -3,7 +3,12 @@ package com.study.library.controller;
 import com.study.library.aop.annotation.ParamsPrintAspect;
 import com.study.library.dto.SignupReqDto;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/auth") // 공통 상위 주소
@@ -11,7 +16,17 @@ public class AuthController {
 
     @ParamsPrintAspect
     @PostMapping("/signup")
-    public ResponseEntity<?> signup(@RequestBody SignupReqDto signupReqDto) {
+    public ResponseEntity<?> signup(@Valid @RequestBody SignupReqDto signupReqDto, BindingResult bindingResult) { // valid체크 후 오류를 bindingResult로 받아옴
+
+        if(bindingResult.hasErrors()) {
+            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+            for(FieldError fieldError : fieldErrors) {
+                String fieldName = fieldError.getField();
+                String message = fieldError.getDefaultMessage();
+                System.out.println(fieldName);
+                System.out.println(message);
+            }
+        }
 
         return ResponseEntity.ok(null);
     }
--- src/main/java/com/study/library/dto/SignupReqDto.java
+++ src/main/java/com/study/library/dto/SignupReqDto.java
@@ -2,10 +2,18 @@ package com.study.library.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
+
 @Data
 public class SignupReqDto {
+
+    @Pattern(regexp = "^[a-z]{1}[a-z0-9]{3,10}+$", message = "영문 숫자 조합 6~10자리여야 합니다.")
     private String username;
+    @Pattern(regexp = "^(?=.*[a-zA-Z])((?=.*\\d)|(?=.*\\W)).{8,128}+$", message = "대소문자, 숫자, 특수문자 조합으로 8 ~ 128자리여야 합니다.")
     private String password;
+    @Pattern(regexp = "^[ㄱ-ㅎ|가-힣]*$", message = "한글로만 입력하여야 합니다.")
     private String name;
+    @Email
     private String email;
 }
