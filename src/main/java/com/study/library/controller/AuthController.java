1615ee684068079345b54f4d1a6350250a0a3aa6
dongyoon7212 <dongyoon7212@naver.com>
2024-03-14 12:05:40 +0900

    feat: valid check

--- src/main/java/com/study/library/controller/AuthController.java
+++ src/main/java/com/study/library/controller/AuthController.java
@@ -8,7 +8,9 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/auth") // 공통 상위 주소
@@ -18,14 +20,15 @@ public class AuthController {
     @PostMapping("/signup")
     public ResponseEntity<?> signup(@Valid @RequestBody SignupReqDto signupReqDto, BindingResult bindingResult) { // valid체크 후 오류를 bindingResult로 받아옴
 
-        if(bindingResult.hasErrors()) {
-            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        if(bindingResult.hasErrors()) { // 에러가 있으면
+            List<FieldError> fieldErrors = bindingResult.getFieldErrors(); // 에러가 있는것들만 가져옴
+            Map<String, String> errorMap = new HashMap<>();
             for(FieldError fieldError : fieldErrors) {
-                String fieldName = fieldError.getField();
-                String message = fieldError.getDefaultMessage();
-                System.out.println(fieldName);
-                System.out.println(message);
+                String fieldName = fieldError.getField(); // DTO 변수명
+                String message = fieldError.getDefaultMessage(); // 에러 메세지
+                errorMap.put(fieldName, message);
             }
+            return ResponseEntity.badRequest().body(errorMap); // 에러가 있으면 응답으로 errorMap을 반환
         }
 
         return ResponseEntity.ok(null);
--- src/main/java/com/study/library/dto/SignupReqDto.java
+++ src/main/java/com/study/library/dto/SignupReqDto.java
@@ -3,17 +3,19 @@ package com.study.library.dto;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 @Data
 public class SignupReqDto {
 
-    @Pattern(regexp = "^[a-z]{1}[a-z0-9]{3,10}+$", message = "영문 숫자 조합 6~10자리여야 합니다.")
+    @Pattern(regexp = "^[a-z]{1}[a-z0-9]{6,14}+$", message = "영문 숫자 조합 7~14자리여야 합니다")
     private String username;
-    @Pattern(regexp = "^(?=.*[a-zA-Z])((?=.*\\d)|(?=.*\\W)).{8,128}+$", message = "대소문자, 숫자, 특수문자 조합으로 8 ~ 128자리여야 합니다.")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])((?=.*\\d)|(?=.*\\W)).{8,128}+$", message = "대소문자, 숫자, 특수문자 조합으로 8 ~ 128자리여야 합니다")
     private String password;
-    @Pattern(regexp = "^[ㄱ-ㅎ|가-힣]*$", message = "한글로만 입력하여야 합니다.")
+    @Pattern(regexp = "^[ㄱ-ㅎ|가-힣]{2,}$", message = "2글자 이상 한글로만 입력하여야 합니다")
     private String name;
+    @NotBlank
     @Email
     private String email;
 }
