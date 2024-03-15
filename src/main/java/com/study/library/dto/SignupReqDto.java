4d5c69231e6bfeb82c964c539e926c79456b636f
dongyoon7212 <dongyoon7212@naver.com>
2024-03-15 12:06:12 +0900

    feat: username duplicate valid check

--- src/main/java/com/study/library/dto/SignupReqDto.java
+++ src/main/java/com/study/library/dto/SignupReqDto.java
@@ -11,11 +11,11 @@ import javax.validation.constraints.Pattern;
 @Data
 public class SignupReqDto {
 
-    @Pattern(regexp = "^[a-z]{1}[a-z0-9]{6,14}+$", message = "영문 숫자 조합 7~14자리여야 합니다")
+    @Pattern(regexp = "^[A-Za-z0-9]{4,14}$", message = "대소문자, 숫자 5 ~ 14자리 형식이어야 합니다.")
     private String username;
-    @Pattern(regexp = "^(?=.*[a-zA-Z])((?=.*\\d)|(?=.*\\W)).{8,128}+$", message = "대소문자, 숫자, 특수문자 조합으로 8 ~ 128자리여야 합니다")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{7,128}$", message = "하나 이상의 영문자, 숫자, 특수문자를 포함한 8 ~ 128자리 형식이어야 합니다.")
     private String password;
-    @Pattern(regexp = "^[ㄱ-ㅎ|가-힣]{2,}$", message = "2글자 이상 한글로만 입력하여야 합니다")
+    @Pattern(regexp = "^[가-힣]{1,}$", message = "2글자 이상 한글로만 입력하여야 합니다")
     private String name;
     @NotBlank
     @Email
