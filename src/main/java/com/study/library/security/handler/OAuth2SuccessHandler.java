752fa0e5d7c744b356421c3bf4afd396dc0e36bd
dongyoon7212 <dongyoon7212@naver.com>
2024-03-21 11:50:49 +0900

    feat: OAuth2 setting

--- src/main/java/com/study/library/security/handler/OAuth2SuccessHandler.java
+++ src/main/java/com/study/library/security/handler/OAuth2SuccessHandler.java
@@ -1,6 +1,11 @@
 package com.study.library.security.handler;
 
+import com.study.library.entity.User;
+import com.study.library.repository.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -12,14 +17,30 @@ import java.io.IOException;
 @Component
 public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
+    @Value("${server.deploy-address}")
+    private String serverAddress;
+    @Value("${server.port}")
+    private String port;
+
+    @Autowired
+    private UserMapper userMapper;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         String name = authentication.getName();
+        User user = userMapper.findUserByOAuth2name(name);
+
+        // OAuth2 로그인을 통해 회원가입이 되어있지 않은 상태 (연동이 된적이 없는 경우)
+        // OAuth2 동기화
+        if(user == null) {
+            DefaultOAuth2User oAuth2User = (DefaultOAuth2User) authentication.getPrincipal();
+            String providerName = oAuth2User.getAttribute("provider").toString();
+
+            response.sendRedirect("http://" + serverAddress + ":" + port + "/");
+            return;
+        }
 
         // OAuth2 로그인을 통해 회원가입을 진행한 기록이 있는지 (연동이 된 경우)
 
-        // OAuth2 로그인을 통해 회원가입이 되어있지 않은 상태 (연동이 된적이 없는 경우)
-
-        // OAuth2 동기화
     }
 }
