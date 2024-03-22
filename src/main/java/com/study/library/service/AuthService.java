374d6f49790861966713f39134b3f835152f679b
dongyoon7212 <dongyoon7212@naver.com>
2024-03-22 09:51:08 +0900

    feat: oAuth2Merge

--- src/main/java/com/study/library/service/AuthService.java
+++ src/main/java/com/study/library/service/AuthService.java
@@ -1,8 +1,10 @@
 package com.study.library.service;
 
+import com.study.library.dto.OAuth2MergeReqDto;
 import com.study.library.dto.OAuth2SignupReqDto;
 import com.study.library.dto.SigninReqDto;
 import com.study.library.dto.SignupReqDto;
+import com.study.library.entity.OAuth2;
 import com.study.library.entity.User;
 import com.study.library.exception.SaveException;
 import com.study.library.jwt.JwtProvider;
@@ -73,6 +75,25 @@ public class AuthService {
         if(successCount < 3) { // 두개 중 하나라도 실패하면 예외처리
             throw new SaveException(); // 런타임 예외 -> 롤백
         }
+    }
+
+    public void oAuth2Merge(OAuth2MergeReqDto oAuth2MergeReqDto) {
+        User user = userMapper.findUserByUsername(oAuth2MergeReqDto.getUsername());
+
+        if(user == null) { // id가 틀림
+            throw new UsernameNotFoundException("사용자 정보를 확인하세요");
+        }
+        if (!passwordEncoder.matches(oAuth2MergeReqDto.getPassword(), user.getPassword())) { // 비밀번호가 틀림
+            throw new BadCredentialsException("사용자 정보를 확인하세요");
+        }
+
+        OAuth2 oAuth2 = OAuth2.builder()
+                .oAuth2Name(oAuth2MergeReqDto.getOauth2Name())
+                .userId(user.getUserId())
+                .providerName(oAuth2MergeReqDto.getProviderName())
+                .build();
+
+        userMapper.saveOAuth2(oAuth2);
     }
 
 }
